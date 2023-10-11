d775face673999a645aaedb958f6dca311e2e61f
Serhii <sergiiriabokon@gmail.com>
2023-10-11 14:04:47 +0300

    removed redundant check

--- customcomissions.php
+++ customcomissions.php
@@ -7,16 +7,11 @@
 /*
 Plugin Name: Custom Comissions
 Plugin URI: http://github.com/sergiiriabokon/customcomissions
-Description: Adds custom comissions to the products price for WooCommerce
+Description: Adds custom commissions to the products price for WooCommerce
 Author: Serge Riabokon
 Version: 0.7.0
 Author URI: http://sergiiriabokon.medium.com
 */
-
-// prohibit direct execution
-if ( !function_exists( 'register_post_type' ) ) {
-	exit('The file is not supposed to be called directly.');
-}
 
 require_once( __DIR__ . '/class-cc-admin.php' );
 require_once( __DIR__ . '/class-cc-product-price.php' );
